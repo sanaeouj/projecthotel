1b8c273db4eed7fd4b70895650ed6d9832345a9f
sanaeouj <sanae.ouajhain@gmail.com>
2021-12-20 05:02:24 +0100

    filtrer

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import Rooms from "./pages/Rooms";
 import ErrorPage from "./pages/ErrorPage";
 import SingleRoom from "./pages/SingleRoom";
 import CustomNavbar from "./components/Customnavbar";
+
 function App() {
   return (
-    <>  
-    <CustomNavbar />
+    <>
+      <CustomNavbar />
       <Router>
         <Routes>
           <Route path="/Home" element={<Home />} />
           <Route path="/SingleRoom" element={<SingleRoom />} />
           <Route path="/Rooms" element={<Rooms />} />
-            <Route path="/ErrorPage" element={<ErrorPage />} />
           <Route path="/ErrorPage">{ErrorPage}</Route>
         </Routes>
       </Router>
--- src/components/CustomCard/type.d.ts
+++ src/components/CustomCard/type.d.ts
@@ -1,5 +1,5 @@
 interface CardProps {
     titre: string;
-    text: string;
+    text?: string;
     children?:any;
   }
